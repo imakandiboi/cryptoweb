1d55ceacaea18605b5246df8124567abba2e9675
imakandiboi <imakandiboi@yahoo.com>
2024-07-02 18:35:55 -0400

    stash dis commit

--- src/components/HomeComponents/ProductComponent.vue
+++ src/components/HomeComponents/ProductComponent.vue
@@ -5,60 +5,18 @@
         <div class="col-md-4">
           <div class="card card-1 mb-3">
             <div class="card-body text-center">
-              <img
-                :src="images.card1"
-                class="card-img-top image mb-3"
-                alt="Credit Card"
-              />
+              <img :src="images.card1" class="card-img-top image mb-3" alt="Credit Card" />
               <h5 class="card-title fw-lighter">Invest when you spend</h5>
-              <p class="card-text">
-                Engaging with crypto involves risks, including the risk of your
-                crypto rewards declining in value over time. Past performance no
-                guarantee of future results. A Gemini Mastercard. Issued by
-                WebBank. Terms apply.
-              </p>
+              <p class="card-text">Engaging with crypto involves risks, including the risk of your crypto rewards declining in value over time. Past performance no guarantee of future results. A Gemini Mastercard. Issued by WebBank. Terms apply.</p>
 
               <div class="card-button">
                 <a href="#" class="btn">Credit Card</a>
                 <picture class="button-pic"
-                  ><source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <img
-                    src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg"
-                    alt="FootIcon-Arrow"
-                    style="object-fit: inherit"
-                    loading="lazy"
-                    decoding="async"
+                  ><source media="(max-width: 767px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(max-width: 767px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <img src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" alt="FootIcon-Arrow" style="object-fit: inherit" loading="lazy" decoding="async"
                 /></picture>
               </div>
             </div>
@@ -67,60 +25,18 @@
         <div class="col-md-4">
           <div class="card card-1 mb-3">
             <div class="card-body text-center">
-              <img
-                :src="images.card2"
-                class="card-img-top image mb-3"
-                alt="Credit Card"
-              />
+              <img :src="images.card2" class="card-img-top image mb-3" alt="Credit Card" />
               <h5 class="card-title fw-lighter">Invest when you spend</h5>
-              <p class="card-text">
-                Engaging with crypto involves risks, including the risk of your
-                crypto rewards declining in value over time. Past performance no
-                guarantee of future results. A Gemini Mastercard. Issued by
-                WebBank. Terms apply.
-              </p>
+              <p class="card-text">Engaging with crypto involves risks, including the risk of your crypto rewards declining in value over time. Past performance no guarantee of future results. A Gemini Mastercard. Issued by WebBank. Terms apply.</p>
 
               <div class="card-button">
                 <a href="#" class="btn">Credit Card</a>
                 <picture class="button-pic"
-                  ><source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <img
-                    src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg"
-                    alt="FootIcon-Arrow"
-                    style="object-fit: inherit"
-                    loading="lazy"
-                    decoding="async"
+                  ><source media="(max-width: 767px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(max-width: 767px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <img src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" alt="FootIcon-Arrow" style="object-fit: inherit" loading="lazy" decoding="async"
                 /></picture>
               </div>
             </div>
@@ -130,60 +46,18 @@
         <div class="col-md-4">
           <div class="card card-1 mb-3">
             <div class="card-body text-center">
-              <img
-                :src="images.card1"
-                class="card-img-top image mb-3"
-                alt="Credit Card"
-              />
+              <img :src="images.card1" class="card-img-top image mb-3" alt="Credit Card" />
               <h5 class="card-title fw-lighter">Invest when you spend</h5>
-              <p class="card-text">
-                Engaging with crypto involves risks, including the risk of your
-                crypto rewards declining in value over time. Past performance no
-                guarantee of future results. A Gemini Mastercard. Issued by
-                WebBank. Terms apply.
-              </p>
+              <p class="card-text">Engaging with crypto involves risks, including the risk of your crypto rewards declining in value over time. Past performance no guarantee of future results. A Gemini Mastercard. Issued by WebBank. Terms apply.</p>
 
               <div class="card-button">
                 <a href="#" class="btn">Credit Card</a>
                 <picture class="button-pic"
-                  ><source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(max-width: 767px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/avif"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <source
-                    media="(min-width: 768px)"
-                    height="48"
-                    width="48"
-                    type="image/webp"
-                    srcset="
-                      https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg
-                    " />
-                  <img
-                    src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg"
-                    alt="FootIcon-Arrow"
-                    style="object-fit: inherit"
-                    loading="lazy"
-                    decoding="async"
+                  ><source media="(max-width: 767px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(max-width: 767px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/avif" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <source media="(min-width: 768px)" height="48" width="48" type="image/webp" srcset="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" />
+                  <img src="https://images.ctfassets.net/jg6lo9a2ukvr/1CfiSzsaaYfUpdoaXDr2uZ/ad1a596b5faed8ca54bcaddba656775c/arrow-btn.svg" alt="FootIcon-Arrow" style="object-fit: inherit" loading="lazy" decoding="async"
                 /></picture>
               </div>
             </div>
@@ -195,34 +69,36 @@
       <div class="word-contain">
         <h2 class="h2cap">A safe place for your crypto</h2>
       </div>
-      <div class="word-contain">
-        <h4 class="h4cap">A safe place for your crypto</h4>
+      <div order="0" class="word-contain">
+        <h4 class="h4cap">We’re focused on providing innovative security solutions to protect your assets.</h4>
+      </div>
+      <div order="0" class="word-contain">
+        <span class="sc-57d69569-1 igZMsG">
+          <a id="register-heading" href="/security">
+            <p class="sc-bbc06255-0 gQulQT cursor-pointer">Learn more about our security measures</p>
+          </a>
+        </span>
       </div>
     </div>
   </div>
 </template>
-  
-  <script>
+
+<script>
 export default {
-  name: "ProductComponent",
+  name: 'ProductComponent',
   data() {
     return {
       images: {
-        card1: require("@/assets/images/card-11.avif"),
-        card2: require("@/assets/images/image-ph.webp"),
+        card1: require('@/assets/images/card-11.avif'),
+        card2: require('@/assets/images/image-ph.webp'),
         //   card3: require('@/assets/nfts.png')
       },
-    };
+    }
   },
-};
+}
 </script>
   
   <style>
-.product-section{
-
-  margin-top: 100px;
-}
-
 .card-img-top {
   width: 100px; /* Adjust as needed */
   margin: 0 auto; /* Center the image */
@@ -252,7 +128,7 @@ export default {
 }
 
 .card-body .image {
-  width: 200px;
+  width: 12 0px;
 }
 
 .card-text {
@@ -308,28 +184,11 @@ export default {
   margin: 0px 0px 8px;
 }
 
-.h2cap {
-  font-size: 48px;
-  color: rgb(77, 79, 79);
-  font-weight: 700;
-  line-height: 48px;
-  text-align: center;
-  letter-spacing: -2.64px;
-  max-width: 896px;
-  margin-bottom: 13px;
-}
-
 .h4cap {
-  font-size: 22px;
-  line-height: 36px;
-  letter-spacing: -1px;
-
+  font-size: 30px;
+  line-height: 60px;
   color: rgb(77, 79, 79);
-  line-height: 32px;
-  text-align: center;
-  letter-spacing: -1px;
-  max-width: 750px;
-  margin-bottom: 16px;
+  letter-spacing: -1.64px;
 }
 
 /* Styles for screens with a minimum width of 1024px */
@@ -354,4 +213,3 @@ export default {
   }
 }
 </style>
-  
